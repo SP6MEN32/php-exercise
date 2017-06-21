4b9d2559dd8a97532710e211bd19aa2e32735207
SP6MEN32 <mixr6@hotmail.fr>
2017-06-21 17:17:28 +0200

    exercice function exo 2

--- function/index.php
+++ function/index.php
@@ -1,7 +1,15 @@
-<?php
+<!-- exercice function exo 1 -->
+<!-- <?php
 function floflo(){
   echo true;
 }
 // rapel la function
 floflo();
+ ?> -->
+<!-- exercice function exo 2 -->
+<?php
+function flo($a){
+  echo $a;
+}
+flo(5);
  ?>
