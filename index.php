8e049fb5cda7ca20d8c13f44a88e8b648a4ffed7
SP6MEN32 <mixr6@hotmail.fr>
2017-06-21 13:58:38 +0200

    exercice loop exo 2

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
-<?php
-for ($i=0; $i <10 ; $i++) { 
+<!-- exercice loop exo1 -->
+<!-- <?php
+for ($i=0; $i <10 ; $i++) {
 	echo "$i";
 };
+?> -->
+<!-- exercice loop exo2 -->
+<?php
+$chiffre1=0;
+$chiffre2=5;
+while ($chiffre1 <= 20) {
+
+	$chiffre3=($chiffre1 * $chiffre2);
+	$chiffre1++;
+	print $chiffre3;
+
+}
+
+
 ?>
