cfba8af6e9eac495d5f96f4f91eb9ff89ed32b50
SP6MEN32 <mixr6@hotmail.fr>
2017-06-21 14:57:55 +0200

    exercice loop exo 4

--- index.php
+++ index.php
@@ -6,26 +6,35 @@ for ($i=0; $i <10 ; $i++) {
 ?> -->
 <!-- exercice loop exo2 -->
 <!-- <?php
-$chiffre1=0;
-$chiffre2=5;
-while ($chiffre1 <= 20) {
+	$chiffre1=0;
+	$chiffre2=5;
+		while ($chiffre1 <= 20) {
 
-	$chiffre3=($chiffre1 * $chiffre2);
-	$chiffre1++;
-	print $chiffre3;
-}
+			$chiffre3=($chiffre1 * $chiffre2);
+			$chiffre1++;
+			print $chiffre3;
+		}
 ?> -->
 <!-- exercice loop exo 3 -->
+<!-- <?php
+	$number1=100;
+	$number2=20;
+		while ($number1 >= 10) {
+			$number3=($number1 * $number2);
+			echo $number3;
+			echo " ";
+			$number1--;
+		}
+?> -->
+<!-- exercice loop exo 4 -->
 <?php
-$number1=100;
-$number2=20;
-while ($number1 >= 10) {
-	$number3=($number1 * $number2);
-	echo $number3;
+$numeroUno=1;
+while($numeroUno <=10) {
+$numeroUno=$numeroUno + ($numeroUno/2);
+	echo $numeroUno;
 	echo " ";
-	$number1--;
-
 }
+
 
 
  ?>
