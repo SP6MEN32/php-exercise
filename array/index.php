f223e113dfa2ea212ef9aae47e5288c2e713b0ec
SP6MEN32 <mixr6@hotmail.fr>
2017-06-22 14:20:25 +0200

    exercice array exo 2,3,4

--- array/index.php
+++ array/index.php
@@ -3,6 +3,27 @@
 $array = array("janvier","fevrier","mars",
 "avril","mai","juin","juillet","aout",
 "septembre","octobre","novembre","decembre");
+  echo '<pre>';
+    print_r ($array);
+  echo '</pre>';
 
-print_r($array);
+//   // exercice array exo 2
+ echo '<pre>';
+ echo $array[2];
+ echo'</pre>';
+
+  //exercice array exo 3
+ echo '<pre>';
+ echo $array[6];
+ echo '</pre>';
+
+  // exercice array exo 4
+// $array[7] = "août";
+//  echo $array;
+  $new = array(7 => "août");
+  $array = array_replace_recursive($array,$new);
+  echo '<pre>';
+  print_r($array);
+  echo '</pre>';
+
  ?>
