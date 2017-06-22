db754695ca6eb3ce7aae352bddda4a78de7bf82b
SP6MEN32 <mixr6@hotmail.fr>
2017-06-22 12:23:52 +0200

    exercice array exo 1

---
+++ array/index.php
@@ -0,0 +1,8 @@
+<!-- exercice array exo 1 -->
+<?php
+$array = array("janvier","fevrier","mars",
+"avril","mai","juin","juillet","aout",
+"septembre","octobre","novembre","decembre");
+
+print_r($array);
+ ?>
