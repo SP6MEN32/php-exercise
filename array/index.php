e849650ae3fe73e6b7558f10c9808d9be3010a5d
SP6MEN32 <mixr6@hotmail.fr>
2017-06-22 16:02:23 +0200

    fin des exercice array

--- array/index.php
+++ array/index.php
@@ -1,34 +1,55 @@
 <!-- exercice array exo 1 -->
 <?php
-$array = array("janvier","fevrier","mars",
+$calendrier = array("janvier","fevrier","mars",
 "avril","mai","juin","juillet","aout",
 "septembre","octobre","novembre","decembre");
 
 //   // exercice array exo 2
  echo '<pre>';
- echo $array[2];
+ echo $calendrier[2];
  echo'</pre>';
 
   //exercice array exo 3
  echo '<pre>';
- echo $array[6];
+ echo $calendrier[6];
  echo '</pre>';
 
   // exercice array exo 4
- $array[7] = "août";
-
   // $new = array(7 => "août");
   // $array = array_replace_recursive($array,$new);
-  echo '<pre>';
-  print_r($array);
-  echo '</pre>';
+  $calendrier[7] = "août";
+  // echo '<pre>';
+  // print_r($array);
+  // echo '</pre>';
 
-//exercice array exo 5
-$departement['dep59'] = "Nord et sa ville de Lille (59)";
-$departement['dep62'] ="Pas de Calais et sa ville de Arras (62)" ;
+//exercice array exo 5+7
+$dep = array(
+  62 => "pas de calais",
+  59 => "Nord",
+  51 => "marne"
+);
+
 
 //exercice array exo6
-echo $departement ['dep59'];
+echo $dep[59].'<br />';
+
+// exercice array exo 8
+for ($numero = 0; $numero < 12; $numero++)
+{
+    echo $calendrier[$numero] . '<br />';
+}
+
+//exercice array exo 9
+foreach($dep as $element)
+{
+    echo $element . '<br />';
+}
+
+
+//exercice array exo 10
+foreach ($dep as $key => $value) {
+  echo "le departement"." ".$value." "."a le numero"." ".$key ."<br />" ;
+}
 
 
 
