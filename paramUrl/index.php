69e4b216c7efba39cf2f1ec124a6657123ce4fd4
SP6MEN32 <mixr6@hotmail.fr>
2017-06-23 10:06:44 +0200

    exercice paramUrl exo 1&2

--- paramUrl/index.php
+++ paramUrl/index.php
@@ -1,8 +1,18 @@
-<!-- exercice paramUrl exo 1 -->
 <?php
+// exercice paramUrl exo 1
   $nom = $_GET['nom'];
   $prenom = $_GET['prenom'];
-  echo ($nom.$prenom);
+  echo ($nom." ".$prenom." ");
+
+// exercice parUrl exo 2
+  $age = $_GET['age'];
+  if ($age) {
+    echo ($age);
+  }
+  else {
+   echo "il y a pas d'age indiquer !";
+  }
+
 
 
  ?>
