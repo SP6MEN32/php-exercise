c829edc03a0d463831243e30e5c3409d3f4195e6
SP6MEN32 <mixr6@hotmail.fr>
2017-06-23 10:51:51 +0200

    exercice paramUrl exo 3&4

--- paramUrl/index.php
+++ paramUrl/index.php
@@ -1,10 +1,12 @@
-<?php
-// exercice paramUrl exo 1
+<!-- <?php
+  // exercice paramUrl exo 1
   $nom = $_GET['nom'];
   $prenom = $_GET['prenom'];
   echo ($nom." ".$prenom." ");
+?> -->
 
-// exercice parUrl exo 2
+<!-- <?php
+  // exercice parUrl exo 2
   $age = $_GET['age'];
   if ($age) {
     echo ($age);
@@ -12,7 +14,26 @@
   else {
    echo "il y a pas d'age indiquer !";
   }
+?> -->
 
+<!-- <?php
+  // exercice paramUrl exo 3
+  $dateDebut = $_GET['dateDebut'];
+  $dateFin = $_GET['dateFin'];
+  echo ("date de début"." ".$dateDebut." "."date de fin"." ".$dateFin);
+?> -->
 
+<!-- <?php
+  //exercice paramUrl exo 4
+  $langage = $_GET['langage'];
+  $serveur = $_GET['serveur'];
 
- ?>
+  if (isset($_GET['langage']) AND isset($_GET['serveur']))
+  {
+  	echo 'Bonjour ' . $_GET['langage'] . ' ' . $_GET['serveur'] . ' !';
+  }
+  else
+  {
+  	echo 'Il faut renseigner un langage et un serveur !';
+  }
+?> -->
