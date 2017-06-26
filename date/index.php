68765496ce866f571de03c716582bebdba77d772
SP6MEN32 <mixr6@hotmail.fr>
2017-06-26 16:21:10 +0200

    exercice date exo 1 & 2 & 3

---
+++ date/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h3>--exercice date exo 1--</h3>
+    <?php
+    echo '<time datetime="'.date('c').'">'.date('d / m / Y').'</time>';
+    ?>
+    <h3>--exercice date exo 2--</h3>
+    <?php
+    echo '<time datetime="'.date('c').'">'.date('d - m - Y').'</time>';
+    ?>
+    <h3>--exercice date exo 3--</h3>
+    <?php
+    setlocale(LC_TIME, 'fr_FR.utf8', 'fra');
+    echo strftime('%A %d %B %Y');
+    ?>
+  </body>
+</html>
