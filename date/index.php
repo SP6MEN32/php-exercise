f94a94118bdb9cdaf73ef6bde183356f318f84dd
SP6MEN32 <mixr6@hotmail.fr>
2017-06-26 17:22:11 +0200

    exercice date exo 5

--- date/index.php
+++ date/index.php
@@ -21,7 +21,19 @@
     <h3>--exercice date exo 4--</h3>
     <?php
     echo strtotime('tuesday 2 august 2016');
+    ?>
+    <h3>--exercice date exo 5--</h3>
+    <?php
 
+    $date1 = strtotime('16 may 2016');
+    $date2 = strtotime('26 june 2017');
+
+    $nbJoursTimestamp = $date2 - $date1;
+
+    //  Pour convertir le timestamp (exprimé en secondes) en jours
+    // On sait que 1 heure = 60 secondes * 60 minutes et que 1 jour = 24 heures donc
+    $nbJours = $nbJoursTimestamp/86400;
+    echo "Nombre de jours : ".$nbJours;
     ?>
   </body>
 </html>
