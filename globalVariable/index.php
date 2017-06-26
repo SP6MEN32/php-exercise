8e49692953dfc06ac7eecb6eeb8799cc814433fd
SP6MEN32 <mixr6@hotmail.fr>
2017-06-26 12:24:53 +0200

    exercice globalVariable exo 3

--- globalVariable/index.php
+++ globalVariable/index.php
@@ -1,3 +1,11 @@
+<?php
+setcookie('login', $_POST['login']);
+setcookie('mdp', $_POST['mdp']);
+
+session_start();
+?>
+
+
 <!-- exercice globalVariable exo 1 -->
 <!DOCTYPE html>
 <html>
@@ -20,10 +28,27 @@
 
 <!-- exercice globalVariable exo 2 -->
 <?php
-  session_start();
+
   $_SESSION['nom'] = 'Argemi(SP6MEN)';
   $_SESSION['prenom'] = 'Florian';
   $_SESSION['age'] = 32;
   echo '<h3>--exercice globalVariable exo 2--</h3>
-  <li><a href="user.php">valider</a></li>';
+  <li><a href="user.php">valider</a></li>
+  <li>
+  <h3>exercice globalVariable exo 3</h3>
+  </li>';
+
+// exercice globalVariable exo 3
+
+  echo '<form action="index.php" method="POST">
+          <label for="login">login</label>
+          <input id="login" name="login" type="text">
+            <label for="mdp">mot de passe</label>
+            <input id="mdp" name="mdp" type="text">
+
+                <button type="submit">valider</button>
+        </form>';
+        $_COOKIE['login'] = $_POST['login'];
+        $_COOKIE['mdp'] = $_POST['mdp'];
+
 ?>
